de4f7b6ef1b3277afcf2552a0919d90dc424f365
brenda <brenda@gargi.(none)>
2008-12-29 12:44:18 +1300

    todo list

---
+++ htdocs/index.php
@@ -0,0 +1,9 @@
+<?php
+
+
+//TODO: Work out method
+//TODO: Check method exists
+//TODO: Work out params
+//TODO Call method
+//TODO Work out form (xml? json? yaml?)
+//TODO Encode as requested and ouput
