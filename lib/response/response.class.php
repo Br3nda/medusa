61f7ad97c5c9d1a6fb706a8989d3181c5d7b3284
Ben Bradshaw <ben@cloud.abutan>
2008-12-29 11:54:10 +1300

    more generic class, not just for errors

--- lib/response/response.class.php
+++ lib/response/response.class.php
@@ -3,14 +3,14 @@
 class error {
         protected $error = array();
 
-        /*
+        /**
          * The return code for the error
          */
         function code($code) {
             assert(!is_null($code));
             $this->error['code'] = $code;
         }
-        /*
+        /**
          * The human readable message
          */
         function message($message) {
@@ -18,7 +18,7 @@ class error {
             $this->error['message'] = $message;
         }
 
-        /*
+        /**
          * Wrapper for code and message so we can do it on one line
          */
         function set($code, $message) {
@@ -26,7 +26,7 @@ class error {
             $this->message($message);
         }
 
-        /*
+        /**
          * So we can append to our error
          */
         function message_append($message) {
@@ -34,16 +34,30 @@ class error {
             $this->error['message'] .= ' ' . $message;
         }
 
-        /*
+        /**
          * Render the error, in whichever format we want
          */
         function render($type = 'html') {
-            if ($type == 'html') {
-                echo $this->error['code'] . ": " . $this->error['message'];
-            }
-            else {
-                echo "Error: Unable to render error";
+            switch ($type) {
+                case 'html':
+                    return $this->__render_html();
+                    break;
+                case 'json':
+                    return $this->__render_json();
+                    break;
+                default:
+                    echo "Error: Unable to render error";
+                    break;
             }
         }
-
+        /*
+         * Private functions - we don't want others calling these directly
+         * Yay for php5!
+         */
+        private function __render_html() {
+            return htmlentities($this->error['code'] . ": " . $this->error['message']);
+        }
+        private function __render_json() {
+            return json_encode($this->error);
+        }
 }
