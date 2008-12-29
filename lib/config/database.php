97ff6ae27e66ea9006effe93cd61f63c557d4c7c
brenda <brenda@wallace.net.nz>
2008-12-29 17:38:12 +1300

    doxygen

--- lib/config/database.php
+++ lib/config/database.php
@@ -1,7 +1,20 @@
 <?php
+/**
+ * @file Database configuration
+*/
+
+/**
+ * @ingroup Database database
+ */
 define("CONFIG_DBTYPE", "pgsql"); # mysql or pgsql
 define("CONFIG_DBHOST", "dewey");
 define("CONFIG_DBPORT", "5433");
 define("CONFIG_DBNAME", "wrms");
 define("CONFIG_DBUSER", "general");
 define("CONFIG_DBPASS", "");
+
+
+/**
+ * @defgroup Database Database
+ * connecting and querying and etc - uses PDO
+ */
