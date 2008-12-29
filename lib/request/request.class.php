2ee38fc40af1d89cd358f42b2d3a84c266d1af67
brenda <brenda@wallace.net.nz>
2008-12-30 11:38:44 +1300

    added request

---
+++ lib/request/request.class.php
@@ -0,0 +1,14 @@
+<?php
+class Request {
+  private $_uri_;
+  function __construct($uri) {
+    $this->_uri_ = $uri;
+  }
+  public function get_method() {
+  }
+  public function get_params() {
+  }
+  public function get_format() {
+  }
+  
+}
