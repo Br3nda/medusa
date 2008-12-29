a1829ba5334110ec94a60d7c16dc3e83d0865cb3
brenda <brenda@wallace.net.nz>
2008-12-29 16:43:43 +1300

    includes the main tag, for api indexing

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,5 +1,15 @@
 <?php
-
+/**
+ * @mainpage {Medusa} the WRMS API
+ *
+ * Requirements
+* Convert existing SQL access, fomr catlayst LAN to Read only - see WRMS/Direct Sql Access 
+* Close off psql access from LAN for the general users (that's the one with the super rights) 
+* Create a new API to encapsulate the business logic of WRMS, codename = Medusa 
+* Convert WRMS web app to use Medusa instead of SQL
+* Set up copy if Ned from Egressive, and get working with Medusa
+* Get other apps (and teh scripts used by catalysters) to all user Medusa 
+ */
 
 //TODO: Work out method
 //TODO: Check method exists
