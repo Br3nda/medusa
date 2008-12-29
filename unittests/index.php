7e76162f9ce32f4880c9c407d049be759e75e396
brenda <brenda@wallace.net.nz>
2008-12-30 11:59:58 +1300

    more tests

--- unittests/index.php
+++ unittests/index.php
@@ -1,9 +1,22 @@
 <?php
+/**
+ * @file Unittests
+ *
+ * @ingroup Unittests
+ */
+
+/**
+ * @defgroup Unittests
+ * @link http://www.simpletest.org/en/start-testing.html Simple test quick start @endlink
+ */
+
+
 require_once('simpletest/autorun.php');
+require_once('../lib/request/request.class.php');
 
-
-require('../lib/request/request.class.php');
-
+/**
+ * @ingroup Unittests
+ */
 class TestRequestParsing extends UnitTestCase {
   function testParsing() {
     $request = new Request('/search.xml?user=brenda');
@@ -16,4 +29,31 @@ class TestRequestParsing extends UnitTestCase {
     $this->assertEqual(sizeof($param), 1);
   }
     
+}
+
+require('../lib/request/request.class.php');
+
+class testSearch extends UnitTestCase {
+  function testSearch() {
+    //TODO
+    //$this->assertTrue(false);
+  }
+}
+
+class testUser extends UnitTestCase {
+  function testStuff() {
+    $user = new user($username, $password);
+    
+  }
+}
+
+class testDatabase extends UnitTestCase {
+  function testConnection() {
+    
+  }
+  function testQuery() {
+    $result = db_query("SELECT * FROM users");
+    $this->assertTrue($result != false);
+    
+  }
 }
