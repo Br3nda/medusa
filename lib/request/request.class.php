aa1c12e5a9c7057bb797edffc919c357e80ff412
Edward Murrell <edwardm@catalyst.net.nz>
2009-01-05 17:11:05 +1300

    Sanity usefulness

--- lib/request/request.class.php
+++ lib/request/request.class.php
@@ -12,6 +12,14 @@ class Request {
   private $_uri_;
   function __construct($uri) {
     $this->_uri_ = $uri;
+    $num = preg_match ('/^\/(\w)\?(.+)$/',$uri,$results);
+    if ($num == 1) {
+        $this->_uri_=$results[0];
+    }
+    $this->_params_ = $_GET; # Frankly, that's all we're really doing
+    $this->_format_ = $_GET['format'];
+    if (! $this->_format_)
+        $this->_format_ = 'xml';
   }
   public function get_method() {
     return $this->_method_;
