c34a8aa101d037c8f4211a3c35171d54b6d71846
Josh Schmidt <joshs@catalyst.net.nz>
2009-01-07 11:32:39 +1300

    Removed orginal config/database.php, remember to copy/rename database.example.php

--- lib/config/database.example.php
+++ lib/config/database.example.php
@@ -7,8 +7,8 @@
  * @ingroup Database database
  */
 define("CONFIG_DBTYPE", "pgsql"); # mysql or pgsql
-define("CONFIG_DBHOST", "dewey");
-define("CONFIG_DBPORT", "5433");
+define("CONFIG_DBHOST", "localhost");
+define("CONFIG_DBPORT", "5432");
 define("CONFIG_DBNAME", "wrms");
 define("CONFIG_DBUSER", "general");
 define("CONFIG_DBPASS", "");
@@ -18,3 +18,4 @@ define("CONFIG_DBPASS", "");
  * @defgroup Database Database
  * connecting and querying and etc - uses PDO
  */
+
