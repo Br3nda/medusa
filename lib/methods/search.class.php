b7dbac11487cf18dcf0c3bbecd1c90463848a816
Edward Murrell <edwardm@catalyst.net.nz>
2009-01-07 10:11:40 +1300

    Actual WRMS objects

---
+++ lib/methods/search.class.php
@@ -0,0 +1,45 @@
+<?php
+
+class search {
+    private $parameters;
+
+    function __construct() {
+    }
+
+    public function run($paramters) {
+        if ($paramters['type'] == null) {
+        	error_logging('WARNING', "No type provided.");
+            return null;
+        }
+        else {
+                $this->parameters = $parameters;
+                switch ($paramters['type']) {
+                case 'request':
+                        return $this->searchWorkRequests();
+                        break;
+                case 'workrequest':
+                        return $this->searchWorkRequests();
+                        break;
+                default:
+                        error_logging('WARNING', "Search type $paramters['type'] doesn't exist.");
+                        break
+                }
+            }
+    }
+
+    private function searchWorkRequests() {
+        $matches = array();
+        $result = db_query("SELECT * FROM request WHERE request_id='%d'", $_GET['id']);
+
+        foreach ($result as $row) {
+            $matches[] = new wrms.WorkRequest()->populate($row);
+        }
+        return $matches;
+    }
+
+    function __destruct() {
+    }
+}
+
+?>
+
