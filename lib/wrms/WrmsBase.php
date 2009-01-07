381801c206f5c0ea547fa6d19212aa31bf8f2c74
brenda <brenda@wallace.net.nz>
2009-01-07 15:51:28 +1300

    code style

--- lib/wrms/WrmsBase.php
+++ lib/wrms/WrmsBase.php
@@ -3,10 +3,8 @@
 /**
  * Base object for all wrms objects, such as work requests, time sheet entries, users, etc
  */
-abstract class WrmsBase
-{
+abstract class WrmsBase {
     // Force Extending class to define this method
     abstract public function populate($row);
 }
 
-?>
