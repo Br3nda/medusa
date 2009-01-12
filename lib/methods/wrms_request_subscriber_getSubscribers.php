a719a9c1062a85c2694b42d059600f846cb58086
Ben Bradshaw <ben@cloud.abutan>
2009-01-13 12:06:41 +1300

    basic implementation - not completely finished

--- lib/methods/wrms_request_subscriber_getSubscribers.php
+++ lib/methods/wrms_request_subscriber_getSubscribers.php
@@ -16,6 +16,28 @@ class wrms_request_subscriber_getSubscribers {
      *    Empty array of failure
      */
     function run($params) {
+        $request_id = $params['GET']['wr'];
+        $access = access::getInstance();
+        if ($access->canUserSeeRequest($request_id)) {
+
+          $result = db_query('SELECT user_no FROM request_interested WHERE request_id = %d', $request_id);
+            if (db_num_rows($result) >= 1) {
+            $users = array();
+            while ($row = db_fetch_object($result)) {
+                $users[] = new user($row->user_no);
+            }
+                $response = new response('Success');
+                $response->set_data('users', $users);
+                return $response;
+            }
+            else {
+                return false;
+            }
+  
+        }
+        else {
+            return new error('Access denied', '403');
+        }
         $return = array();
         return $return;
     }
