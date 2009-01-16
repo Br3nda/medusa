9213017c37735af9ba0f3ea52acc107317969d0d
brenda <brenda@wallace.net.nz>
2009-01-16 15:15:15 +1300

    fixed code style

--- lib/logging/logging.inc.php
+++ lib/logging/logging.inc.php
@@ -1,7 +1,7 @@
 <?php
 function error_logging($type, $message) {
 	//TODO only log if debug level reached
-	if(preg_match('!^DEBUG!', $type) && !DEBUG_MODE) {
+	if (preg_match('!^DEBUG!', $type) && !DEBUG_MODE) {
 		return;
 	}
 	error_log("$type $message");
