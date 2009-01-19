01492630ebcc650abed698d52a8b81c9b6fc9d24
Brenda Wallace <brenda@wallace.net.nz>
2009-01-20 11:41:32 +1300

    fixed the test for error responseop

--- unittests/index.php
+++ unittests/index.php
@@ -335,9 +335,12 @@ class renderertest extends UnitTestCase {
 
     $xml = $response_renderer->render('xml');
 
-    
-    if (!$this->assertEqual("<response>\n<error>\n<code>200</code>\n<message></message>\n<data></data>\n<status_message> does not exist</status_message>\n<status_code>400</status_code>\n</error>\n</response>", $xml)) {
-      $this->dump($xml);
+    $xml = preg_replace('!\s!', '', $xml);
+    $correct = preg_replace('!\s!', '', "<error><status><message>doesnotexist</message><code>400</code></status><data></data></error>");
+    if (!$this->assertEqual($correct, $xml)) {
+      //what we g
+      $this->dump('I got this: '. $xml);
+      $this->dump('I expected this: '. $correct);
     }
   }
 
