753a5f80951386e8c9990c520597339d877fe57a
Brenda Wallace <brenda@wallace.net.nz>
2009-01-26 13:28:45 +1300

    doxygen

--- lib/methods/wrms_base_method.php
+++ lib/methods/wrms_base_method.php
@@ -1,4 +1,14 @@
 <?php
+
+/**
+ * @defgroup Methods Methods
+ * Callable by restful api
+ */
+ 
+/**
+ * Base class for all methods
+ * @ingroup Methods
+ */
 class wrms_base_method {
   function run($params) {
     
