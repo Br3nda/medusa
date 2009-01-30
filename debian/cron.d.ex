68ee04235d9386011f5a0ac16d683853afd0c58d
Edward Murrell <edwardm@catalyst.net.nz>
2009-01-30 14:53:55 +1300

    Added the debian/ directory

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the medusa package
+#
+
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(medusa):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="medusa" command="/usr/bin/medusa"
