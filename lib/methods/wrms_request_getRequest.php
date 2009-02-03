b3a20b2717994b06930a75e2acc5ff931182af55
Joshua Waihi <josh@lupe.(none)>
2009-02-04 10:08:08 +1300

    get request mad eto handle arrays

--- lib/methods/wrms_request_getRequest.php
+++ lib/methods/wrms_request_getRequest.php
@@ -10,13 +10,13 @@
   * @ingroup Methods
   * Work Requests
   */
-class wrms_request_getRequest extends wrms_base_method {
+class wrms_request_getRequests extends wrms_base_method {
     /**
      * Performs the fetch of the work request
      *
      * @param $params
      *   Associative array of parameters
-     *   - $params->wr: Work Request ID
+     *   - $params->wr: Work Request ID or array of
      *   - $params->user: User ID making the request
      *   @return
      *     The request object on success
@@ -24,20 +24,31 @@ class wrms_request_getRequest extends wrms_base_method {
      *     NULL if no work request
      */
     function run($params) {
-      $request_id = $params['GET']['wr'];
-      $access = access::getInstance();
-      if ($access->canUserSeeRequest($request_id)) {
-        $result = db_query('SELECT * FROM request WHERE request_id = %d', $request_id);
-        if (db_num_rows($result) == 1) {
-          $response = new response('Success');
-          $object = new WrmsWorkRequest();
-          $object->populate(db_fetch_object($result));
-          $response->set('wr', $object);
-          return $response;
-        }
-
-        
+      $requests = $params['GET']['wr'];
+      if (!is_array($requests)) {
+        $requests = array($requests);
       }
-      return new Error('Request does not exist or you do not have permission to view it');
+      $access = access::getInstance();
+      $wr = array();
+      foreach ($requests as $request_id) {
+        if ($access->canUserSeeRequest($request_id)) {
+          $wr[$request_id] = null;
+        }
+      }
+      $placeholders = array();
+      $placeholders = array_pad($placeholders, count($wr), '%d');
+      $sql = 'SELECT * FROM request WHERE request_id IN (' . implode(', ', $placeholders)  . ')';
+      $result = call_user_func_array('db_query', array_unshift($sql, $wr));
+      if (!db_num_rows($result)) {
+        return new Error('Request does not exist or you do not have permission to view it');
+      }
+      $response = new response('Success');
+      while ($row = db_fetch_objet($result)) {
+        $object = new WrmsWorkRequest();
+        $object->populate($row);
+        $wr[$row->request_id] = $object;
+      }
+      $response->set('wr', $wr);
+      return $response;
     }
 }
