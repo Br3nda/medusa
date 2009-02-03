cdcd55ca0d402ba37a5a98027e09015403e7786e
Joshua Waihi <josh@lupe.(none)>
2009-02-04 10:48:40 +1300

    fixed multi issues for get requests ?wr=1234,4321,23498...

--- lib/methods/wrms_request_getRequests.php
+++ lib/methods/wrms_request_getRequests.php
@@ -24,10 +24,7 @@ class wrms_request_getRequests extends wrms_base_method {
      *     NULL if no work request
      */
     function run($params) {
-      $requests = $params['GET']['wr'];
-      if (!is_array($requests)) {
-        $requests = array($requests);
-      }
+      $requests = explode(',', $params['GET']['wr']);
       $access = access::getInstance();
       $wr = array();
       foreach ($requests as $request_id) {
@@ -38,12 +35,13 @@ class wrms_request_getRequests extends wrms_base_method {
       $placeholders = array();
       $placeholders = array_pad($placeholders, count($wr), '%d');
       $sql = 'SELECT * FROM request WHERE request_id IN (' . implode(', ', $placeholders)  . ')';
-      $result = call_user_func_array('db_query', array_unshift($sql, $wr));
+      $params = array($sql) + array_keys($wr);
+      $result = call_user_func_array('db_query', $params);
       if (!db_num_rows($result)) {
         return new Error('Request does not exist or you do not have permission to view it');
       }
       $response = new response('Success');
-      while ($row = db_fetch_objet($result)) {
+      while ($row = db_fetch_object($result)) {
         $object = new WrmsWorkRequest();
         $object->populate($row);
         $wr[$row->request_id] = $object;
